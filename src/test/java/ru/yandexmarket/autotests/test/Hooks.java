6e435cc13051490225960eaaa234ff162d8abeeb
Sergeis87 <sergeis87@mail.ru>
2023-02-26 23:38:03 +0300

    Part works

--- src/test/java/ru/yandexmarket/autotests/test/Hooks.java
+++ src/test/java/ru/yandexmarket/autotests/test/Hooks.java
@@ -3,6 +3,7 @@ package ru.yandexmarket.autotests.test;
 import com.codeborne.selenide.Configuration;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import static com.codeborne.selenide.Selenide.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -14,16 +15,17 @@ public class Hooks {
 
         Configuration.browserSize = "1920x1080";
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-extensions");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        Configuration.browserCapabilities = capabilities;
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-extensions");
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//        Configuration.browserCapabilities = capabilities;
     }
 
     @After
     public void tearDown(){
         // WebDriverRunner.getWebDriver().close();
+        closeWebDriver();
     }
 }
